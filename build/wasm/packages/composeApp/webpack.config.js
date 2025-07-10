8c6ff28d084230818beae0e73154ccb4f97e483e
Jokwanhee <jonotch1@naver.com>
2025-07-11 02:09:26 +0900

    fix github actions script

--- build/wasm/packages/composeApp/webpack.config.js
+++ build/wasm/packages/composeApp/webpack.config.js
@@ -1,5 +1,5 @@
 let config = {
-  mode: 'development',
+  mode: 'production',
   resolve: {
     modules: [
       "node_modules"
@@ -26,13 +26,14 @@ config.output = {
     clean: true,
     globalObject: "globalThis"
 };
+config.output.path = require('path').resolve(__dirname, "../../../../composeApp/build/kotlin-webpack/wasmJs/productionExecutable")
 // source maps
 config.module.rules.push({
         test: /\.m?js$/,
         use: ["source-map-loader"],
         enforce: "pre"
 });
-config.devtool = 'eval-source-map';
+config.devtool = 'source-map';
 config.ignoreWarnings = [
     /Failed to parse source map/,
     /Accessing import\.meta directly is unsupported \(only property access or destructuring is supported\)/
@@ -42,17 +43,9 @@ config.ignoreWarnings = [
 config.devServer = {
   "open": true,
   "static": [
-    "kotlin",
-    "../../../../composeApp/build/processedResources/wasmJs/main",
     "/Users/jogwanhui/kmp/Jokwanhee.github.io",
     "/Users/jogwanhui/kmp/Jokwanhee.github.io/composeApp"
-  ],
-  "client": {
-    "overlay": {
-      "errors": true,
-      "warnings": false
-    }
-  }
+  ]
 };
 
 // noinspection JSUnnecessarySemicolon
