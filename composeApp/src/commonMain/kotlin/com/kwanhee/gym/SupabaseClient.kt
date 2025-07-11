3f65f565676b9f72d45e4a6912a59fce51dccc98
Jokwanhee <jonotch1@naver.com>
2025-07-12 00:50:47 +0900

    fix error

--- composeApp/src/commonMain/kotlin/com/kwanhee/gym/SupabaseClient.kt
+++ composeApp/src/commonMain/kotlin/com/kwanhee/gym/SupabaseClient.kt
@@ -6,10 +6,9 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
     val client = createSupabaseClient(
-        supabaseUrl = ApiKeys.supabaseUrl,
-        supabaseKey = ApiKeys.supabaseKey
+        supabaseUrl = "https://qhgkskqotndjcaadvstx.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZ2tza3FvdG5kamNhYWR2c3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjkwNTUsImV4cCI6MjA2NzM0NTA1NX0.ZM_xKmUddGDMCJBdwGhVXofoueh8J1__H5OpoFkKEqY"
     ) {
         install(Postgrest)
-//        install(GoTrue)
     }
 }
