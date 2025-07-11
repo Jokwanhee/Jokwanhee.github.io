dcb0a7f9a29bcdd85d8138dd9fd84b1894cc046b
Jokwanhee <jonotch1@naver.com>
2025-07-12 01:06:12 +0900

    fix

---
+++ composeApp/src/commonMain/kotlin/com/kwanhee/gym/Key.kt
@@ -0,0 +1,6 @@
+package com.kwanhee.gym
+
+object Key {
+    const val SupabaseUrl = "https://qhgkskqotndjcaadvstx.supabase.co"
+    const val SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZ2tza3FvdG5kamNhYWR2c3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjkwNTUsImV4cCI6MjA2NzM0NTA1NX0.ZM_xKmUddGDMCJBdwGhVXofoueh8J1__H5OpoFkKEqY"
+}
--- composeApp/src/commonMain/kotlin/com/kwanhee/gym/SupabaseClient.kt
+++ composeApp/src/commonMain/kotlin/com/kwanhee/gym/SupabaseClient.kt
@@ -1,13 +1,12 @@
 package com.kwanhee.gym
 
-import com.kwanhee.gymapp.const.ApiKeys
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
     val client = createSupabaseClient(
-        supabaseUrl = "https://qhgkskqotndjcaadvstx.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZ2tza3FvdG5kamNhYWR2c3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjkwNTUsImV4cCI6MjA2NzM0NTA1NX0.ZM_xKmUddGDMCJBdwGhVXofoueh8J1__H5OpoFkKEqY"
+        supabaseUrl = Key.SupabaseUrl,
+        supabaseKey = Key.SupabaseKey
     ) {
         install(Postgrest)
     }
