8258d3b514fac8282ecea2f2d52fdfe29efecb6c
Jokwanhee <jonotch1@naver.com>
2025-07-13 18:16:13 +0900

    .

--- composeApp/src/wasmJsMain/resources/amplitude.js
+++
@@ -1,4 +0,0 @@
-// Copyright 2025 dev.kwan
-window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
-window.amplitude.init('cb66ff451a5cbfea6d6be3abc369c011', {"autocapture":{"elementInteractions":true}});
-
