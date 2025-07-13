0a6de965141492bdfae0b60c7c11b3a0fa4b813a
Jokwanhee <jonotch1@naver.com>
2025-07-13 18:07:51 +0900

    add amplitude key

---
+++ composeApp/src/wasmJsMain/resources/amplitude.js
@@ -0,0 +1,4 @@
+// Copyright 2025 dev.kwan
+window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
+window.amplitude.init('cb66ff451a5cbfea6d6be3abc369c011', {"autocapture":{"elementInteractions":true}});
+
